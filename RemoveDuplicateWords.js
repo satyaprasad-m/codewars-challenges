c784cb888e666782a498119264028676dd0de0d0
Satyaprasad <satyap257@gmail.com>
2019-08-20 00:30:27 +0530

    Reduced code using Set()

--- RemoveDuplicateWords.js
+++ RemoveDuplicateWords.js
@@ -1,12 +1,13 @@
 function removeDuplicateWords(s) {
-  let words = [];
-  s.split(" ").forEach(function(word) {
-    if (!words.includes(word)) {
-      words.push(word);
-    }
-  });
-  return words.join(" ");
+  //   let words = [];
+  //   s.split(" ").forEach(function(word) {
+  //     if (!words.includes(word)) {
+  //       words.push(word);
+  //     }
+  //   });
+  //   return words.join(" ");
+  return Array.from(new Set(s.split(" "))).join(" ");
 }
-removeDuplicateWords(
+console.log(removeDuplicateWords(
   "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
-);
+));
