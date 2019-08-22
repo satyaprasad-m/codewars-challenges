0dd3a18f3b6f9dd4e1eb4a34488aa1c7a6581207
Satyaprasad <satyap257@gmail.com>
2019-08-22 20:51:45 +0530

    22-08-2019

---
+++ numberToString.js
@@ -0,0 +1,13 @@
+function numberToString(num) {
+  return num.toString();
+}
+function numberToString(num) {
+  return num + "";
+}
+function numberToString(num) {
+  return String(num);
+}
+function numberToString(num) {
+  return `${num}`;
+}
+console.log(numberToString(67) === "67");
